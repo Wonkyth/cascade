cc3de3a299c896ee7b5f1967b1c1842c3d164609
Wonkyth <wonkyth@gmail.com>
2020-07-30 10:38:26 +1000

    add basic setTaskStatus functionality

--- app.js
+++ app.js
@@ -5,14 +5,13 @@ let taskCount = 5;
 newSavedTask.addEventListener("click", saveButtonClicked);
 
 function saveButtonClicked() {
-  //   console.log("save button clicked");
-  const newTaskName = document.querySelector("#newTaskName").nodeValue;
+  const newTaskName = document.querySelector("#newTaskName").value;
   const newTaskDescription = document.querySelector("#newTaskDescription")
     .value; //Ok prettier
   const newTaskAssignee = document.querySelector("#newTaskAssignee");
   const newTaskDate = document.querySelector("#newTaskDate");
   const newTaskTime = document.querySelector("#newTaskTime");
-  const newTaskStatus = document.querySelector("#newTaskStatus"); //todo: how to get the value from option tag
+  const newTaskStatus = document.querySelector("#newTaskStatus").value; //todo: how to get the value from option tag
 
   console.log({
     newTaskName,
@@ -32,9 +31,34 @@ function saveButtonClicked() {
   );
 }
 
+// task: element
+// status: string
 function setTaskStatus(task, status) {
   //todo: do all this
-  console.error("this ain't implemented yet, ya dingus!");
+  const taskProgress = task.querySelector(".progress-bar");
+  switch (parseInt(status, 10)) {
+    case 1:
+      taskProgress.innerHTML = "To Do";
+      break;
+    case 2:
+      taskProgress.innerHTML = "In Progress";
+      break;
+    case 3:
+      taskProgress.innerHTML = "Awaiting Review";
+      break;
+    case 4:
+      taskProgress.innerHTML = "Done!";
+      break;
+
+    default:
+      console.error(
+        `Status "${status}" does not exist! Setting status text to "ERROR".`
+      );
+      taskProgress.innerHTML = "ERROR";
+      taskProgress.setAttribute("class", "progress-bar bg-danger");
+      break;
+  }
+  console.warn("this ain't fully implemented yet, ya dingus!");
 }
 
 function addTask(name, description, date, time, assignee, status) {
@@ -42,14 +66,20 @@ function addTask(name, description, date, time, assignee, status) {
   const newTask = template.content.firstElementChild.cloneNode(true);
   taskCount++;
 
+  //set id of task
+  newTask.id = `taskID${taskCount}`;
+
   const collapser = newTask.querySelector(".collapser");
   collapser.setAttribute("data-target", `#taskCollapsable${taskCount}`);
 
   const collapsable = newTask.querySelector("#templateCollapsable");
   collapsable.id = `taskCollapsable${taskCount}`;
 
-  //todo: set name
+  const taskName = newTask.querySelector(".templateName");
+  taskName.innerText = name;
   //todo: set desc
+  const taskDescription = newTask.querySelector(".templateDescription");
+  taskDescription.innerText = description;
   //todo: set date
   //todo: set time (possibly same info as date?)
   //todo: set assignee
